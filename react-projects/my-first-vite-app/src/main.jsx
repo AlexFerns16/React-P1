cebf59e844d6ed575feb46648a21055d0ef3814c
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-06-19 17:56:22 +0530

    commit:18 actual DOM and virtual DOM

--- react-projects/my-first-vite-app/src/main.jsx
+++ react-projects/my-first-vite-app/src/main.jsx
@@ -1,14 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// function based component
 function FuncCompo() {
   return (
     <h1>
-      <div>Hello React!</div>
+      <div>Hello React - Function Based Component - VDOM!</div>
     </h1>
   );
 }
 
+// for function based component
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(<FuncCompo />);
+
+// using actual DOM
+const root_1 = document.getElementById("root_1");
+
+// using virtual DOM
+const root_2 = ReactDOM.createRoot(document.getElementById("root_2"));
+
+//
+root_1.innerHTML = `
+  <div>
+    <h1>Hello JS - ADOM!</h1>
+  </div>
+`;
+
+//
+root_2.render(
+  <div>
+    <h1>Hello React - VDOM!</h1>
+  </div>
+);
