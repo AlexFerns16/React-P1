95078cea5f43f23c26cadde3dc1161dbb4e94c87
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-06-19 18:39:27 +0530

    commit:20 setInterval

--- react-projects/my-first-vite-app/src/main.jsx
+++ react-projects/my-first-vite-app/src/main.jsx
@@ -21,8 +21,10 @@ const root_1 = document.getElementById("root_1");
 // using virtual DOM
 const root_2 = ReactDOM.createRoot(document.getElementById("root_2"));
 
-//
-root_1.innerHTML = `
+// using setInterval for refreshing date and time after every 1000 ms (1 sec)
+setInterval(() => {
+  //
+  root_1.innerHTML = `
   <div>
     <h1>Hello JS - ADOM!</h1>
     <input />
@@ -30,18 +32,19 @@ root_1.innerHTML = `
   </div>
 `;
 
-//
-const username = "Alex";
-root_2.render(
-  <div>
-    <h1>Hello React - VDOM!</h1>
-    <input />
-    <pre>
-      {/* data binding > the data in the variable is binded to the html elements*/}
-      {username} {}
-      {/* string interpolation > data within brackets is converted to string */}
-      {4 + 5} {Math.random()} {}
-      {new Date().toLocaleTimeString()}
-    </pre>
-  </div>
-);
+  //
+  const username = "Alex";
+  root_2.render(
+    <div>
+      <h1>Hello React - VDOM!</h1>
+      <input />
+      <pre>
+        {/* data binding > the data in the variable is binded to the html elements*/}
+        {username} {}
+        {/* string interpolation > data within brackets is converted to string */}
+        {4 + 5} {Math.random()} {}
+        {new Date().toLocaleTimeString()}
+      </pre>
+    </div>
+  );
+}, 1000);
