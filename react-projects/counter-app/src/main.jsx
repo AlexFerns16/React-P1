45874a8c5a0dba3c82ff12a9a428ab7a5896b818
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-06-26 18:32:49 +0530

    commit:27 debugging virtual DOM and Function

--- react-projects/counter-app/src/main.jsx
+++ react-projects/counter-app/src/main.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // associated to method 1
-function myFunction() {
-  console.log(Math.random());
-}
+// function myFunction() {
+//   console.log(Math.random());
+// }
 
 function Button() {
-  let counter = 0;
-
   //   method 1: function 'myFunction' as a value to the 'onClick' property
   //   --------------------------------------------------------------------------------------
-  return <button onClick={myFunction}>{counter}</button>;
+  //   return <button onClick={myFunction}>{counter}</button>;
 
   //   method 2: directly defining an anonymous function as a value to the 'onClick' property
   //   --------------------------------------------------------------------------------------
@@ -27,7 +25,23 @@ function Button() {
 
   //   method 3: directly defining an arrow function as a value to the 'onClick' property
   //   --------------------------------------------------------------------------------------
-  //   return <button onClick={() => console.log(Math.random())}>{counter}</button>;
+
+  // initializing the counter
+  let counter = 0;
+
+  // incrementing the counter function
+  const setCounter = () => {
+    counter = counter + 1;
+    // debugging the increment counter function
+    console.log(`debug inside setCounter function, count: ${counter}`);
+  };
+
+  // debugging the virtual DOM
+  console.log(`debug inside Button() function, count: ${counter}`);
+
+  // return
+  return <button onClick={setCounter}>{counter}</button>;
 }
 
+// creating a virtual DOM
 ReactDOM.createRoot(document.getElementById("root")).render(<Button />);
