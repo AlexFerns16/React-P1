d2c09b92db6b5aeb0d4a4549795ebfac9d56e599
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-06-26 23:15:16 +0530

    commit:28 useState( )

--- react-projects/counter-app/src/main.jsx
+++ react-projects/counter-app/src/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 
 // associated to method 1
@@ -26,21 +26,22 @@ function Button() {
   //   method 3: directly defining an arrow function as a value to the 'onClick' property
   //   --------------------------------------------------------------------------------------
 
-  // initializing the counter
-  let counter = 0;
-
-  // incrementing the counter function
-  const setCounter = () => {
-    counter = counter + 1;
-    // debugging the increment counter function
-    console.log(`debug inside setCounter function, count: ${counter}`);
+  // useState
+  // every 'hook' in react starts with 'use'
+  // 'hook' can be called anytime in the lifecycle of a component
+  // whenever the state value changes, it maintains the state of a variable
+  // in our case the state is nothing but the 'counter' value
+  // returns a stateful value, and a function to update it in an array, [stateful value, function]
+  const [counter, setCounter] = useState(0);
+  const handleclick = () => {
+    setCounter(counter + 1);
   };
 
   // debugging the virtual DOM
   console.log(`debug inside Button() function, count: ${counter}`);
 
   // return
-  return <button onClick={setCounter}>{counter}</button>;
+  return <button onClick={handleclick}>{counter}</button>;
 }
 
 // creating a virtual DOM
