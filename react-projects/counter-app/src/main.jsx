b83f13b69d1be23d35cd76334325701e27e80072
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-06-26 13:59:07 +0530

    commit:26 different methods of assigning a function to the 'onClick' property

--- react-projects/counter-app/src/main.jsx
+++ react-projects/counter-app/src/main.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// associated to method 1
 function myFunction() {
   console.log(Math.random());
 }
 
 function Button() {
   let counter = 0;
+
+  //   method 1: function 'myFunction' as a value to the 'onClick' property
+  //   --------------------------------------------------------------------------------------
   return <button onClick={myFunction}>{counter}</button>;
+
+  //   method 2: directly defining an anonymous function as a value to the 'onClick' property
+  //   --------------------------------------------------------------------------------------
+  //   return (
+  //     <button
+  //       onClick={function () {
+  //         console.log(Math.random());
+  //       }}
+  //     >
+  //       {counter}
+  //     </button>
+  //   );
+
+  //   method 3: directly defining an arrow function as a value to the 'onClick' property
+  //   --------------------------------------------------------------------------------------
+  //   return <button onClick={() => console.log(Math.random())}>{counter}</button>;
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(<Button />);
